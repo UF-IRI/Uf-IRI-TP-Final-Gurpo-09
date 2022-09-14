0bb8a4b68ed67c8610ab6ecbba44729f23216752
eastanganelli <ezequielstanganelli@gmail.com>
2022-09-14 13:34:26 -0300

    Action MacOS testing

--- unit-test/casos_base.cpp
+++ unit-test/casos_base.cpp
@@ -5,18 +5,18 @@
 using namespace testing;
 
 namespace Casos_Base::tests {
-	TEST(Casos_Base, Caso_I) {
+	TEST(Casos_Base, Test_Caso1) {
 		foobar::Example example{};
         ASSERT_THAT(example.getValue(), Eq(88));
 	}
 
-	TEST(Casos_Base, Caso_II) {
+	TEST(Casos_Base, Test_Caso2) {
 		foobar::Example example{};
         ASSERT_THAT(example.getValue(), Eq(88));
 	}
 
-	TEST(Casos_Base, Caso_III) {
+	TEST(Casos_Base, Test_Caso3) {
 		foobar::Example example{};
-        ASSERT_THAT(example.getValue(), Eq(88));
+        ASSERT_THAT(example.getValue(), Eq(99));
 	}
 }
